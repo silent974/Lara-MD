ff796b720428f7252ec933777959d4f0780cdec2
silent974 <emmanuelmillions12345@gmail.com>
2024-12-30 02:07:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "LmoUkBwJ#f6TPwaYAMBs5oio43_e4Ii7AFfv0J-JR1acPJGqnTqo", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
